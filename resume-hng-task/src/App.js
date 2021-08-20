9e459e04bea826f412e02e9f954ec3e02f3ef35a
abisola <abisola.ca@gmail.com>
2021-08-20 16:27:04 +0100

    fixed link highlight issue

--- resume-hng-task/src/App.js
+++ resume-hng-task/src/App.js
@@ -6,8 +6,17 @@ import Skills from './components/skills';
 import Education from './components/education';
 import Footer from './components/footer';
 import MobileMenu from './components/mobile-menu';
+import { useState, useEffect } from 'react';
+import Loading from './components/loading';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(()=> {
+      setTimeout(()=> {
+        setIsLoading(false)
+      }, 2000)
+  }, [])
+
   const showMenu = () => {
     let menu = document.querySelector(".mobile-menu")
     
@@ -18,6 +27,10 @@ function App() {
       menu.setAttribute("style", "right: -1000px;");
     }
 };
+
+if(isLoading){
+  return <Loading/>
+}
 
   return (
     <div className="App">
